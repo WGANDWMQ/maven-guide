a6687cb19ae6be38f28b32d8db26dd745d08a59c
wanggang2632@qianmi.com <wanggang2632@qianmi.com>
2019-01-18 14:23:50 +0800

    maven guide

---
+++ maven-module/src/main/java/com/geek/maven/guide/MavenModuleApplication.java
@@ -0,0 +1,14 @@
+package com.geek.maven.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenModuleApplication.class, args);
+	}
+
+}
+
